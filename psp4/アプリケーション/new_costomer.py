e17afa0cdd769b99d761aac712683c0b1e453c8b
Souta3 <bb38121015@ms.nagasaki-u.ac.jp>
2023-07-14 15:42:54 +0900

    q

--- psp4/アプリケーション/new_costomer.py
+++ psp4/アプリケーション/new_costomer.py
@@ -1,7 +1,14 @@
+import Table
+
 table_list = [[]]
-tables = 4
-for i in range(tables):
-    table_list.append(Table_name)
-    Table_name = Table_name + 1
+
+
+
+def ManageCostomerInformation(table_name, number_of_people, age):
+    new = Table.Table(table_name, 0, number_of_people, age, 0)
+    newninfo = new.Table_list()
+    table_list[table_name][] = newinfo
+
+ManageCostomerInformation(2, 4, 20)
 
 print(table_list)
