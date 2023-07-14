96e43a6efebf3dee99d9b3e023603bf5a9330aed
Souta3 <bb38121015@ms.nagasaki-u.ac.jp>
2023-07-14 14:00:59 +0900

    a

---
+++ psp4/アプリケーション/bashing.py
@@ -0,0 +1,14 @@
+class Table:
+    def __init__(self, table_name, bashing_status, number_of_people, age, total_amount):
+        self.table_name = table_name
+        self.bashing_status = 0
+        self.number_of_people = number_of_people
+        self.age = age
+        self.total_amount = total_amount
+
+    def ReleaceBashingStatus(self):
+        self.bashing_status = 0
+
+    def CallTable():
+
+def CheckBashing(table_name, bashing_table):
